3dedfd9d83a20f5d6f7bccbd8859b2635fc46dfc
Alex Fernandez <a.fernandez@mayanx.com>
2014-03-17 23:42:02 -0600

    CreateMetadataTable CreateMetadataField <= [Doing] CreateMetadataTableHeader UpdateGeneralHeader

--- main.cpp
+++ main.cpp
@@ -26,6 +26,18 @@ int main(int argc, char *argv[])
     name.copy(system.DatabaseName,sizeof(system.DatabaseName),0);
 
     bool result=tbspace.UpdateSystemBlock((char*)&system);
+    char numbers[]={'0','1','2','3','4','5'};
+    for(int x=0; x<5 ; x++){
+        char tableName[]={'a','l','u','m','n','o','s',numbers[x]};
+
+        long lastId= tbspace.CreateTable(tableName);
+
+        TableMetadataHeader header;
+        char * rawData= tbspace.GetTableMetadataHeader(lastId);
+        memcpy(&header, rawData, sizeof(TableMetadataHeader));
+        cout << "Table Name: " <<  header.TableName;
+        printf("Last table created: %d \n", lastId);
+    }
 
     return a.exec();
 }
